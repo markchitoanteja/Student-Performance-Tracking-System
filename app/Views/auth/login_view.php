478c410435aab2b2934f3dbbf6afb216335b5fae
MarkChito <00anteja23@gmail.com>
2024-12-05 20:10:39 +0800

    Modify Login View

--- app/Views/auth/login_view.php
+++ app/Views/auth/login_view.php
@@ -81,7 +81,7 @@
                     <i class="fas fa-2x fa-sync fa-spin"></i>
                 </div>
                 <div class="modal-header">
-                    <h5 class="modal-title">Account Settings</h5>
+                    <h5 class="modal-title">New Admin</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
